49ff40fd1d8f12f4f0ead02b86defe519c73610f
caoaolong <eason105cc@163.com>
2024-11-14 10:22:48 +0800

    修复换行符

--- src/kernel32/kernel.c
+++ src/kernel32/kernel.c
@@ -6,7 +6,6 @@
 void csos_init(memory_info_t* mem_info, uint32_t gdt_info)
 {
     tty_init();
-    tty_printf("Hello, %d!", 3434);
-
+    tty_printf("Hello,World,%d!\nHello,%s\n", 13, "CSOS");
     while(TRUE);
 }
